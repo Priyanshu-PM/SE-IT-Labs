47dd6b7d83d2854701a209985a8f8e26e204b34d
Priyanshu-PM <priyanshumahukhaye@gmail.com>
2023-05-27 20:26:17 +0530

    windmill animation

---
+++ CGL_Practicals/WindMill.cpp
@@ -0,0 +1,92 @@
+//PROBLEM STATEMENT:-WRITE A PROGRAME IN OPENGL ON LINUX PERFORM TO ANIMATE ANY ONE SCENE (WIND MILL)
+
+#include <GL/glut.h>
+#include <bits/stdc++.h>
+#include<math.h>
+
+using namespace std;
+
+int frameNumber = 0;
+
+void drawWindMill()
+{
+    int i;
+
+    glColor3f(1.0 , 1.0, 0.0);
+
+    glBegin(GL_POLYGON);
+    glVertex2f(-0.05, 0);
+    glVertex2f(-0.05, 3);
+    glVertex2f(0.05, 3);
+    glVertex2f(0.05, 0);
+    glEnd();
+
+    glTranslatef(0, 3, 0);
+    glColor3f(1.0, 0.0, 0.0);
+
+    glRotated(frameNumber*(180.0/45), 0, 0, 1);
+
+    for(i = 0; i < 4; i++)
+    {
+        glRotated(90, 0, 0, 1);
+        glBegin(GL_POLYGON);
+        glVertex2f(0.0, 0.0);
+        glVertex2f(1.0, 0.2);
+        glVertex2f(1.0, -0.2);
+        glEnd();
+    }
+}
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+
+    glPushMatrix();
+    glTranslated(2.2, 1.6, 0);
+    glScaled(0.4, 0.4, 1);
+    drawWindMill();
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslated(3.7, 0.8, 0);
+    glScaled(0.7, 0.7, 1);
+    drawWindMill();
+    glPopMatrix();
+
+    glutSwapBuffers();
+
+}
+
+void doFrame(int v)
+{
+    frameNumber++;
+    glutPostRedisplay();
+    glutTimerFunc(10, doFrame, 0);
+}
+
+void initialize(void)
+{
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glLoadIdentity();
+    glMatrixMode(GL_PROJECTION);
+    glOrtho(0, 7, -1, 4, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+
+}
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE);
+    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(700, 500);
+    glutCreateWindow("WindMill");
+
+    initialize();
+    glutDisplayFunc(display);
+    glutTimerFunc(200, doFrame, 0);
+    glutMainLoop();
+
+    return 0;
+}
