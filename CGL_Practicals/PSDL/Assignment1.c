ae49d8e7f377cdd7173d9113139a8b80157d80a2
Priyanshu-PM <priyanshumahukhaye@gmail.com>
2023-06-05 11:11:29 +0530

    psdl 2 added

--- CGL_Practicals/PSDL/Assignment1.c
+++ CGL_Practicals/PSDL/Assignment1.c
@@ -8,11 +8,20 @@
 
 void main(void)
 {
+
+    //  initializing the registers
     TMR0 = 0x09;
     TMR1 = 0x05;
+
+    //  performing the addition of two registers
     TMR2 = TMR0 + TMR1;
 
+    //  setting port A as output
     TRISA = 0;
+
+    //  displaying at port A
     PORTA = TMR2;
+
+    //  end of program
     return;
 }
--- CGL_Practicals/PSDL/Assignment2.c
+++ CGL_Practicals/PSDL/Assignment2.c
@@ -3,23 +3,37 @@
  * Name : Priyanshu
 */
 
+//  including the header files
+
 #include <xc.h>
 #include <pic18f458.h>
 
 void main(void)
 {
+
+    // bank select register (0 selected by default)
     BSR = 0x00;
+
+    // creating an array of integers
     int arr[100];
     int sum = 0;
 
     for(int i = 0; i< 100; i++)
     {
+        //  storing the numbers in sum variable
         arr[i] = 1;
         sum += arr[i];
     }
 
+    //  initializing the TMR0 with sum
     TMR0 = sum;
+
+    //  For demonstration purpose
     BSR = 0x01;
+
+    //  reinitializing the sum variable
     sum = 100;
+
+    //  end of program
     return;
 }
