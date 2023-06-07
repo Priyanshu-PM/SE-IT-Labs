a083b4718fb2b65f746122e775b9ce1db25369f0
Priyanshu-PM <priyanshumahukhaye@gmail.com>
2023-06-07 15:55:30 +0530

    swastik proper

--- CGL_Practicals/swastik.cpp
+++ CGL_Practicals/swastik.cpp
@@ -22,6 +22,16 @@ float r, g, b;
 };
 
 
+Color f_color = {1.0, 0.0, 0.0};
+Color b_color = {0.0, 0.0, 0.0};
+
+Color getPixelColor(int x, int y)
+{
+Color c;
+glReadPixels(x, y, 1.0, 1.0, GLUT_RGB, GL_FLOAT, &c);
+return c;
+}
+
 vector<pair<double, double>> createObject()
 {
 vector<pair<double, double>> points =
@@ -61,6 +71,24 @@ ans = points;
 return ans;
 }
 
+
+void boundaryFill(int x, int y)
+{
+Color c = getPixelColor(x, y);
+if((c.r != b_color.r || c.r != b_color.g || c.b != b_color.b) && (c.r != f_color.r || c.g != f_color.g || c.b != f_color.b))
+{
+glBegin(GL_POINTS);
+glColor3f(f_color.r, f_color.g, f_color.b);
+glVertex2f(x, y);
+glEnd();
+glFlush();
+boundaryFill(x+1, y);
+boundaryFill(x-1, y);
+boundaryFill(x, y+1);
+boundaryFill(x, y-1);
+}
+
+}
 
 void displayPoint(float x, float y)
 {
@@ -130,9 +158,33 @@ displayPoint(x, y);
 }
 }
 
+void floodFill(int x, int y)
+{
+
+cout << "Inside the flood fill ";
+
+Color old = {1.0, 1.0, 1.0};
+Color c = getPixelColor(x, y);
+
+if(c.r == old.r && c.g == old.g && c.b == old.b)
+{
+glBegin(GL_POINTS);
+glColor3f(1.0, 0.0, 0.0);
+glVertex2f(x, y);
+glEnd();
+glFlush();
+floodFill(x+1, y);
+floodFill(x-1, y);
+floodFill(x, y+1);
+floodFill(x, y-1);
+}
+glFlush();
+
+}
+
 void drawObject()
 {
-glPointSize(2);
+//glPointSize(2);
 for(int i = 0; i < points.size()-1; i++)
 {
 double x1 = ans[i].first;
@@ -154,7 +206,7 @@ bresenhams(x1, y1, x2, y2);
 }
 
 
-void rotateObject(double x, double y, double angle, vector<pair<double, double>> &points)
+void rotateObject(double x, double y, vector<pair<double, double>> &points)
 {
     angle = (angle * 3.14159) / 180.0; // Convert angle to radians
 
@@ -177,21 +229,40 @@ void rotateObject(double x, double y, double angle, vector<pair<double, double>>
     }
 }
 
+
+void keyboard(char unsigned key, int x, int y)
+{
+if(key == 'r')
+{
+initialize();
+rotateObject(x, y, points);
+drawObject();
+
+        f_color.r = 1.0;
+        f_color.g = 0.0;
+        f_color.b = 0.0;
+boundaryFill(400, 400);
+
+}
+}
+
 void mouseListener(int button, int state, int x, int y)
 {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
     {
         cout<<" x: "<<x<<" y: "<<y<<endl;
-        vector<pair<double,double>> points = createObject();
-        rotateObject(x,y,45,points);
-        glColor3f(0.0, 1.0, 0.0);
+        cout << "Enter the rotation angle : ";
+        cin >> angle;
+        points = createObject();
+        rotateObject(x,y,points);
+        glColor3f(0.0, 0.0, 0.0);
+        glClear(GL_COLOR_BUFFER_BIT);
         drawObject();
+        floodFill(400, 400);
         glFlush();
+       
     }
-}
-void floodFill(int x, int y)
-{
-return;
+    //glutKeyboardFunc(keyboard);
 }
 
 void primitives()
--- PSDL/Assignment9.c
+++ PSDL/Assignment9.c
@@ -107,4 +107,5 @@ int main(void)
     }
 
     return 0;
+    
 }
